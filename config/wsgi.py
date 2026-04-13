4d8d5b20fa1ca2143609bb048a51b657f8842076
shagorrobidas <roysagor88@gmail.com>
2026-04-14 00:48:18 +0600

    fix : wasig

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,6 +1,17 @@
+# import os
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
+
+# application = get_wsgi_application()
 import os
-from django.core.wsgi import get_wsgi_application
+import sys
+
+project_home = '/home/shagorrobidasjvai/artica-fullstack'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
